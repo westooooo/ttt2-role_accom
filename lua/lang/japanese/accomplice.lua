ed8cee3a820f9825463258b92bbf9bc052a863f2
westooooo <68987820+westooooo@users.noreply.github.com>
2021-03-02 12:21:48 +0900

    Add files via upload

---
+++ lua/lang/japanese/accomplice.lua
@@ -0,0 +1,11 @@
+L = LANG.GetLanguageTableReference("日本語")
+
+-- GENERAL ROLE LANGUAGE STRINGS
+L[ACCOMPLICE.name] = "Accomplice"
+L["info_popup_" .. ACCOMPLICE.name] = [[人狼でいう狂人、Accompliceになった！
+主人を探し、勝利へ導こう！]]
+L["body_found_" .. ACCOMPLICE.abbr] = "奴はAccompliceだったな！"
+L["search_role_" .. ACCOMPLICE.abbr] = "こいつはAccpliceだったようだな！"
+L["target_" .. ACCOMPLICE.name] = "Accomplice"
+L["ttt2_desc_" .. ACCOMPLICE.name] = [[Accompliceとは、簡単に言えばトレイターの仲間。
+トレイター達からは誰がAccompliceなのかわかるが、Accompliceからは誰がトレイターなのかわからない。能力や行動で見分けよう！]]
